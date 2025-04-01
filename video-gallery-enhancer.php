c1247a03b37ab66606e56d259494631d84d72055
AnkushK2022 <111953971+AnkushK2022@users.noreply.github.com>
2025-04-01 22:19:16 +0530

    Update video-gallery-enhancer.php

--- video-gallery-enhancer.php
+++ video-gallery-enhancer.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Video Gallery Enhancer Pro
 Description: Add video to WooCommerce product gallery with position control with Custom thumbnail option.
 Version: 1.0
-Author: ankushk2022
+Author: AnkushK2022
 */
 
 // Add admin meta box
